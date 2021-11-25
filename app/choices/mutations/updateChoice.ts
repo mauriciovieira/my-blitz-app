4efe875384a5e31a483aa43ef47b62ce9529f4cb
Mauricio Vieira <mauricio@mauriciovieira.net>
2021-11-25 19:47:17 -0300

    feat: allow votes to be counted

--- app/choices/mutations/updateChoice.ts
+++ app/choices/mutations/updateChoice.ts
@@ -11,8 +11,10 @@ export default resolver.pipe(
   resolver.zod(UpdateChoice),
   resolver.authorize(),
   async ({ id, ...data }) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-    const choice = await db.choice.update({ where: { id }, data })
+    const choice = await db.choice.update({
+      where: { id },
+      data: { votes: { increment: 1 } },
+    })
 
     return choice
   }
